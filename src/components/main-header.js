cc813641b5fb191949df0cd6f95c9e3889fb5fe9
Sudaraka Wijesinghe <sudaraka@sudaraka.org>
2016-01-19 20:30:24 +0530

    04 - foundation

---
+++ src/components/main-header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import MainNav from './main-nav';
+
+class MainHeader extends React.Component {
+  render() {
+    return <div className='main-header'>
+      <div className='logo'>Storekeeper</div>
+      <MainNav />
+    </div>;
+  }
+}
+
+export default MainHeader;
---
+++ src/components/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Page extends React.Component {
+  render() {
+    return <div className='page'>
+      {this.props.children}
+    </div>;
+  }
+}
+
+export default Page;
